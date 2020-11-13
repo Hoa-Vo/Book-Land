8a1454bcfd090b048f452a64c968d87e36852598
AN VÕ THIỆN <voan0935995700@gmail.com>
2020-11-13 22:07:01 +0700

    binding new image

--- app.js
+++ app.js
@@ -23,7 +23,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/", indexRouter);
 app.use("/login", loginRouter);
 app.use("/register", registerRouter);
-app.use("/bookslist",booksListRouter); 
+app.use("/bookslist",booksListRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
