7a52955eb5731604e7c42b3a04be350df59f2382
Hoa-Vo <dev.h1620@gmail.com>
2020-12-06 13:44:20 +0700

    up file

--- controllers/booksListController.js
+++ controllers/booksListController.js
@@ -6,3 +6,7 @@ exports.listing = async (req, res, next) => {
   // Pass data to view to display list of books
   res.render("booksPage/bookslist", { books });
 };
+exports.paging= async  (req,res,next)=>{
+  const data  = await  booksListModel.paging(req.query.page,req.query.pagelimit);
+  res.send({data});
+}
