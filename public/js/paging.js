a17430f4e6766b96e803e79434362a20da5b586c
Hoa-Vo <dev.h1620@gmail.com>
2020-12-07 15:24:11 +0700

    limit pagenumber

--- public/js/paging.js
+++ public/js/paging.js
@@ -1,31 +1,41 @@
-let booksPerPage = 6;
+let booksPerPage = 3;
 let totalBook;
-let maxPage;
 let currentPage = 1;
+let maxPage=0;
+let pageNumberBar;
+let allBtn="";
 $(document).ready(async () => {
-  await fetchData(currentPage,booksPerPage);
+  fetchData(currentPage,booksPerPage);
 });
 function prevBtnClick() {
   if (currentPage > 1) {
     currentPage--;
-    fetchData(currentPage, booksPerPage);
+    fetchData(currentPage, booksPerPage,1);
+    toogleActive(currentPage+1);
   }
 }
 function  nextBtnClick(){
   if (currentPage < maxPage) {
     currentPage++;
-    fetchData(currentPage,booksPerPage);
+    fetchData(currentPage,booksPerPage,2);
+    toogleActive(currentPage-1);
   }
 }
 function inThisPage(currentPage){
   const id = `page-${currentPage}`;
-  console.log(id);
   document.getElementById(id).classList.add("active");
 }
-function setPage(num){
-  fetchData(parseInt(num),6);
+function toogleActive(currentPage){
+  const id = `page-${currentPage}`;
+  document.getElementById(id).classList.remove("active");
 }
-async function fetchData (currentPage,booksPerPage){
+function setPage(num){
+   toogleActive(currentPage);
+  currentPage=parseInt(num);
+  fetchData(currentPage,booksPerPage,0);
+}
+async function fetchData (currentPage,booksPerPage,isPrevNextClick){
+  $("#book-list").html("<div id='loading'>Loading...</div>");
   $.ajax({
     url: "http://localhost:3000/bookslist/page",
     type: "GET",
@@ -37,7 +47,10 @@ async function fetchData (currentPage,booksPerPage){
       let content = "";
       let image;
       totalBook = res.data.totalBook;
-      maxPage = parseInt(totalBook/booksPerPage);
+      if(maxPage===0) {
+        maxPage = Math.ceil(totalBook / booksPerPage);
+      }
+      console.log(maxPage)
       res.data.books.forEach(book => {
         image = `<img src='images/booksImage/${book.image_link}'class='img-fluid book-image' alt='Image'>`;
         content +=
@@ -48,24 +61,45 @@ async function fetchData (currentPage,booksPerPage){
             "<a href=`/bookslist/' data-toggle='tooltip' data-placement='right' title='Xem'>" +
             "<i class='fas fa-eye'></i></a></li><li><a href='#' data-toggle='tooltip' data-placement='right' title='So sánh'>" +
             "<i class='fas fa-sync-alt'></i></a></li><li><a href='#' data-toggle='tooltip' data-placement='right' title='Thêm vào yêu thích'>" +
-            "<iclass=\"far fa-heart\"></i></a></li></ul><a class='cart' href='#'>Thêm vào giỏ hàng</a></div></div><div class='why-text'>" +
+            "<iclass='far fa-heart'></i></a></li></ul><a class='cart' href='#'>Thêm vào giỏ hàng</a></div></div><div class='why-text'>" +
             "<h4>" +
             book.title +
             "</h4><h5>" +
             book.basePrice +
             "VND</h5></div></div></div>";
       });
-      let allBtn="";
-      let pageNumberBar =" <li class='page-item'> <a id='prevBtn' onclick='prevBtnClick()' class='page-link' aria-label='Previous'> <span aria-hidden='true'>&laquo;</span> <span class='sr-only'>Previous</span> </a> </li>"
-      for(let i= 1;i<=maxPage;i++){
-        allBtn+=`<li  id='page-${i}' class='page-item'><a onclick='setPage(this.textContent)' class='page-link'>${i}</a></li>`;
+      if(isPrevNextClick) {
+        let btnArr;
+        if(isPrevNextClick==2) {
+          btnArr = listOfButtonNext(currentPage, 5, maxPage);
+        }else{
+          btnArr = listOfButtonPrev(currentPage, 5, maxPage);
+        }
+        pageNumberBar="empty";
+        if(btnArr.length) {
+         updatePagination(btnArr);
+        }
       }
-      allBtn+="<li class='page-item'> <a id='nextBtn' onclick='nextBtnClick()' class='page-link' aria-label='Next'> <span aria-hidden='true'>&raquo;</span> <span class='sr-only'>Next</span> </a> </li>"
-      pageNumberBar+=allBtn
+      if(!pageNumberBar){
+        btnArr=[1,2,3,4,5];
+        updatePagination(btnArr);
+      }
       $("#book-list").html(content);
-      $(".pagination").html(pageNumberBar);
       inThisPage(currentPage);
     },
     error: function (jqXHR, textStatus, err) {},
   });
 };
+function  updatePagination(btnArr){
+  console.log(maxPage);
+  console.log(btnArr);
+  allBtn="";
+  pageNumberBar = " <li class='page-item'> <a id='prevBtn' onclick='prevBtnClick()' class='page-link' aria-label='Previous'> <span aria-hidden='true'>&laquo;</span> <span class='sr-only'>Previous</span> </a> </li>"
+  for (let i = 0; i < btnArr.length; i++) {
+    if(btnArr[i]<=maxPage)
+    allBtn += `<li  id='page-${btnArr[i]}' class='page-item'><a onclick='setPage(this.textContent)' class='page-link'>${btnArr[i]}</a></li>`;
+  }
+  allBtn += "<li class='page-item'> <a id='nextBtn' onclick='nextBtnClick()' class='page-link' aria-label='Next'> <span aria-hidden='true'>&raquo;</span> <span class='sr-only'>Next</span> </a> </li>"
+  pageNumberBar += allBtn
+  $(".pagination").html(pageNumberBar);
+}
