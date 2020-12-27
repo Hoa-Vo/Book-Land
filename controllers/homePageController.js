3a83df9c955889e37f3d701d12a2a05ca6a4d281
AN VÕ THIỆN <voan0935995700@gmail.com>
2020-12-27 22:45:12 +0700

    done: basic sign in

--- controllers/homePageController.js
+++ controllers/homePageController.js
@@ -1,12 +1,19 @@
-exports.renderHomePage = (req, res, next) => {
+const accountModel = require("../models/accountModel");
+exports.renderHomePage = async (req, res, next) => {
   let isSignedIn = false; 
+  let userToShow = null ;
   if(req.user)
   {
     console.log(`req.user: ${req.user._id}`); 
     isSignedIn =  true; 
+    userToShow = await accountModel.getUserById(req.user._id); 
+    console.log(userToShow); 
   }
 
   
   // render based on logged in or not
-  res.render("./homepage/index");
+  res.render("./homepage/index",{
+    isSignedIn: isSignedIn,
+    userToShow: userToShow
+  });
 };
