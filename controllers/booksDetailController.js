6e04dcb8927662942bb1172390958477dc147432
truongtrieu123 <uerittruong@gmail.com>
2020-12-27 17:06:26 +0700

    update controller

--- controllers/booksDetailController.js
+++ controllers/booksDetailController.js
@@ -3,12 +3,17 @@ const booksModel = require("../models/booksModel");
 exports.listing = async (req, res, next) => {
   // Get books from model
   const book = await booksModel.get(req.params.id);
+  const comments=await booksModel.fetchAllComments(req.params.id);
+  const commentCount=await booksModel.commentCount(req.params.id);
   // Pass data to view to book detail
   res.render("bookDetailsPage/booksDetail", {
+    id:book._id,
     title: book.title,
     basePrice: book.basePrice,
     publisher: book.publisher,
     author: book.author,
     imageLink: book.image_link,
+    comments:comments,
+    commentcount:commentCount,
   });
 };
--- routes/bookslist.js
+++ routes/bookslist.js
@@ -3,11 +3,14 @@ const router = express.Router();
 const booksListController = require("../controllers/booksListController");
 const booksDetailController = require("../controllers/booksDetailController");
 const searchSpecifiedBookController=require("../controllers/searchSpecifiedBookController");
+const commentController =require("../controllers/commentsController");
 // Get booklist page
 router.get("/", booksListController.listing);
+router.post("/:id/comment",commentController.ReceivedComments);
 router.get("/search", searchSpecifiedBookController.get);
 router.get("/page",booksListController.paging);
 router.get("/:id", booksDetailController.listing);
+router.get("/:id/allcomments",commentController.getAllComments);
 router.delete("/", (req, res, next) => {
   console.log(req.query.ID);
 });
