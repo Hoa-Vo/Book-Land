89c481b36d70e3ba6c4393efc816eb8e70f04178
Hoa-Vo <dev.h1620@gmail.com>
2020-12-28 16:04:21 +0700

    minor bug

--- controllers/cartController.js
+++ controllers/cartController.js
@@ -1,3 +1,12 @@
+const accountModel = require("../models/accountModel");
+
 exports.renderCartPage = async (req, res, next) => {
-  res.render("./cartPage/cart");
+  let userToShow = null;
+  if (req.user) {
+    console.log(`req.user: ${req.user._id}`);
+    isSignedIn = true;
+    userToShow = await accountModel.getUserById(req.user._id);
+    console.log(userToShow);
+  }
+  res.render("./cartPage/cart", userToShow);
 };
