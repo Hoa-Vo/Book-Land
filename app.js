a4c464b031cc5af2089f16b7e1daec20b17250be
Hoa-Vo <dev.h1620@gmail.com>
2020-12-30 01:40:51 +0700

    check user credential

--- app.js
+++ app.js
@@ -15,6 +15,7 @@ const registerRouter = require("./routes/register");
 const booksListRouter = require("./routes/bookslist");
 const accountRouter = require("./routes/account");
 const cartRouter = require("./routes/cart");
+const checkOutRouter = require("./routes/checkout");
 require("./database/db");
 // view engine setup
 app.set("views", path.join(__dirname, "views"));
@@ -43,12 +44,12 @@ app.use("/login", loginRouter);
 app.use("/register", registerRouter);
 app.use("/bookslist", booksListRouter);
 app.use("/account", accountRouter);
-app.get("/logout", (req,res) => 
-{
-   req.logOut();
-   res.redirect("/");
-} )
+app.get("/logout", (req, res) => {
+  req.logOut();
+  res.redirect("/");
+});
 app.use("/cart", cartRouter);
+app.use("/checkout", checkOutRouter);
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
   next(createError(404));
