b1a5f3727915359199632b89e286ba1550bab52e
truongtrieu123 <uerittruong@gmail.com>
2020-12-29 11:52:46 +0700

    validate email, validate input

--- public/js/comment.js
+++ public/js/comment.js
@@ -6,29 +6,28 @@ const maxItemsPerPage=6;
 
 window.onload=function(){
     let currentPage=1;
-let maxPage=0;
-let totalItems=0;
-const maxItemsPerPage=6;
+    let maxPage=0;
+    let totalItems=0;
+    const maxItemsPerPage=6;
     $(document).ready(function()  {
         load_comment_paging();
+        $("#alert-success").hide();
+        $("#alert-danger").hide();
     });
 }
 
 async function insert_comment(){
-    const yourname=document.getElementById("YourName").value;
-    const youremail=document.getElementById('YourEmail').value;
-    const yourthoughts =document.getElementById("YourThoughts").value;
-    const bookID=document.getElementById("bookId").textContent;
-
     $( "#comment-form" ).click(function( event ) {
         event.preventDefault();
     });
-    if(yourname=="" ||youremail==""|| yourthoughts=="")
-    {
-        //alert  missing information
-        validateInput();
-    }
-    else{
+
+    var check=validateInput();
+
+    if(check==true){
+        const yourname=document.getElementById("YourName").value;
+        const youremail=document.getElementById('YourEmail').value;
+        const yourthoughts =document.getElementById("YourThoughts").value;
+        const bookID=document.getElementById("bookId").textContent;
         const url='/bookslist/'+bookID+'/send-comment';
         await $.ajax({
             url: url,
@@ -48,6 +47,9 @@ async function insert_comment(){
                     currentPage=1;
                     refreshCommentPost();
                     load_comment_paging();
+                    $("#alert-success").text("Thêm bình luận thành công");
+                    $("#alert-danger").hide();
+                    $("#alert-success").show();
                 }
                 return false;
             },
@@ -79,6 +81,7 @@ async function load_comment_paging()
                 totalItems=response.count;
                 setPageNumber();
                 updateCommentsList(response.commentlist,response.count);
+                
             }
             return false;
         },
@@ -93,7 +96,47 @@ async function load_comment_paging()
 
 function validateInput()
 {
+    var yourname=document.getElementById("YourName").value;
+    var youremail=document.getElementById('YourEmail').value;
+    var yourthoughts =document.getElementById("YourThoughts").value;
+    
+    var result=true;
+    $("#alert-success").hide();
+    if(yourthoughts=="" || yourthoughts==null||yourthoughts==undefined){
+        $("#alert-danger").show();
+        $("#alert-danger").text("Thiếu nội dung bình luận");
+        result=false;
+    }
+    else if(yourname=="" || yourname==null||yourname==undefined){
+        $("#alert-danger").text("Bạn chưa nhập tên");
+        $("#alert-danger").show();
+        result=false;
+    }
+    else if(youremail=="" || youremail==null||youremail==undefined){
+        $("#alert-danger").text("Bạn chưa nhập email");
+        $("#alert-danger").show();
+        result=false;
+    }
+    else if(validateEmail(youremail)==false){
+        $("#alert-danger").text("Email không đúng cấu trúc");
+        $("#alert-danger").show();
+        result=false;
+    }
+    return result;
+}
 
+function validateEmail(email)
+{
+    var result=true;
+    var mailformat = /^[a-zA-Z0-9.!#$%&'*+/=?^_`{|}~-]+@[a-zA-Z0-9-]+(?:\.[a-zA-Z0-9-]+)*$/;
+
+    if (email.match(mailformat)){
+       result=true; 
+    }
+    else{
+        result=false;
+    }
+    return result;
 }
 
 function setPageNumber()
