0e5da469032b6a2ea643d4a76ceba578f15f216e
truongtrieu123 <uerittruong@gmail.com>
2020-12-30 18:03:49 +0700

    update comment.js

--- public/js/comment.js
+++ public/js/comment.js
@@ -2,6 +2,8 @@
 let currentPage=1;
 let maxPage=0;
 let totalItems=0;
+let userToShow;
+let isSingedIn=false;
 const maxItemsPerPage=6;
 
 window.onload=function(){
@@ -11,8 +13,7 @@ window.onload=function(){
     const maxItemsPerPage=6;
     $(document).ready(function()  {
         load_comment_paging();
-        $("#alert-success").hide();
-        $("#alert-danger").hide();
+        load_comment_post();
     });
 }
 
@@ -24,10 +25,11 @@ async function insert_comment(){
     var check=validateInput();
 
     if(check==true){
-        const yourname=document.getElementById("YourName").value;
-        const youremail=document.getElementById('YourEmail').value;
-        const yourthoughts =document.getElementById("YourThoughts").value;
         const bookID=document.getElementById("bookId").textContent;
+        let userInfo=[];
+        if(isSingedIn==true) userInfo=userToShow;
+        else if(isSingedIn==false) userInfo=get_anonymous_user_info();
+        const yourthoughts=document.getElementById("YourThoughts").value;
         const url='/bookslist/'+bookID+'/send-comment';
         await $.ajax({
             url: url,
@@ -35,17 +37,20 @@ async function insert_comment(){
             data:
             {
                 bookId: bookID,
-                name: yourname,
-                email:youremail,
                 content: yourthoughts,
+                isSignedIn:isSingedIn,
+                userId:userInfo.id,
+                userName:userInfo.name,
+                userEmail:userInfo.email,
             },
             success: function(response)
             {
+                console.log(response.count);
                 if(true)
                 {
                     totalItems=response.count;
                     currentPage=1;
-                    refreshCommentPost();
+                    refresh_comment_post();
                     load_comment_paging();
                     $("#alert-success").text("Thêm bình luận thành công");
                     $("#alert-danger").hide();
@@ -60,7 +65,7 @@ async function insert_comment(){
             }   
         });
     }
-
+    return false;
 }
 
 async function load_comment_paging()
@@ -80,8 +85,7 @@ async function load_comment_paging()
             if (true){
                 totalItems=response.count;
                 setPageNumber();
-                updateCommentsList(response.commentlist,response.count);
-                
+                update_comment_list(response.commentlist,response.count);
             }
             return false;
         },
@@ -92,35 +96,72 @@ async function load_comment_paging()
     })
 }
 
+function update_comment_list(commentList,count) {
+    //update comment frame and paging;
+    const source = $("#commentsList").html();
+    const template = Handlebars.compile(source);
+    $("#comments-list").html(template(commentList));
+    document.getElementById("comment-count-3").textContent=count;
+    document.getElementById("comment-count-2").textContent=count;
+    document.getElementById("comment-count-1").textContent=count;
+}
 
+function load_comment_post()
+{
+    let check=is_signed_in()
+    $("#alert-success").hide();
+    $("#alert-danger").hide();
+    if(check==false){
+        const source = $("#comment-anonymous").html();
+        const template = Handlebars.compile(source);
+        $("#comment-frame").html(template());
+    }
+    else
+    {
+        const source = $("#comment-loged-in").html();
+        const template = Handlebars.compile(source);
+        $("#comment-frame").html(template(userToShow));
+    }
+}
 
 function validateInput()
 {
-    var yourname=document.getElementById("YourName").value;
-    var youremail=document.getElementById('YourEmail').value;
-    var yourthoughts =document.getElementById("YourThoughts").value;
-    
     var result=true;
     $("#alert-success").hide();
-    if(yourthoughts=="" || yourthoughts==null||yourthoughts==undefined){
-        $("#alert-danger").show();
-        $("#alert-danger").text("Thiếu nội dung bình luận");
-        result=false;
+    if(isSingedIn==true)
+    {
+        var yourthoughts =document.getElementById("YourThoughts").value;
+        if(yourthoughts=="" || yourthoughts==null||yourthoughts==undefined){
+            $("#alert-danger").show();
+            $("#alert-danger").text("Thiếu nội dung bình luận");
+            result=false;
+        }
     }
-    else if(yourname=="" || yourname==null||yourname==undefined){
-        $("#alert-danger").text("Bạn chưa nhập tên");
-        $("#alert-danger").show();
-        result=false;
-    }
-    else if(youremail=="" || youremail==null||youremail==undefined){
-        $("#alert-danger").text("Bạn chưa nhập email");
-        $("#alert-danger").show();
-        result=false;
-    }
-    else if(validateEmail(youremail)==false){
-        $("#alert-danger").text("Email không đúng cấu trúc");
-        $("#alert-danger").show();
-        result=false;
+    else if(isSingedIn==false){
+        var yourname=document.getElementById("YourName").value;
+        var youremail=document.getElementById('YourEmail').value;
+        var yourthoughts =document.getElementById("YourThoughts").value;
+
+        if(yourthoughts=="" || yourthoughts==null||yourthoughts==undefined){
+            $("#alert-danger").show();
+            $("#alert-danger").text("Thiếu nội dung bình luận");
+            result=false;
+        }
+        else if(yourname=="" || yourname==null||yourname==undefined){
+            $("#alert-danger").text("Bạn chưa nhập tên");
+            $("#alert-danger").show();
+            result=false;
+        }
+        else if(youremail=="" || youremail==null||youremail==undefined){
+            $("#alert-danger").text("Bạn chưa nhập email");
+            $("#alert-danger").show();
+            result=false;
+        }
+        else if(validateEmail(youremail)==false){
+            $("#alert-danger").text("Email không đúng cấu trúc");
+            $("#alert-danger").show();
+            result=false;
+        }
     }
     return result;
 }
@@ -160,15 +201,7 @@ function setPageNumber()
     }
 }
 
-function updateCommentsList(list,count) {
-    const source = $("#commentsList").html();
-    const template = Handlebars.compile(source);
-    $("#comments-list").html(template(list));
-    document.getElementById("comment-count-3").textContent=count;
-    document.getElementById("comment-count-2").textContent=count;
-    document.getElementById("comment-count-1").textContent=count;
-}
-function refreshCommentPost()
+function refresh_comment_post()
 {
     document.getElementById("YourName").value="";
     document.getElementById('YourEmail').value="";
@@ -196,4 +229,29 @@ function setPage(num) {
     currentPage = parseInt(num);
     setPageNumber();
     load_comment_paging();
+}
+
+
+function is_signed_in ()
+{
+    //lay user fomr layout.hbs
+    let userId=$("#user-id").text();
+    let userName=$("#user-name").text();
+    let userEmail=$("#user-email").text();
+    if (userId == ""||userId==undefined||userId==null)  {
+        userToShow=null;
+        isSingedIn=false;
+    }
+    else{
+        userToShow={id:userId,name:userName, email: userEmail};
+        isSingedIn=true;
+    }
+    return isSingedIn;
+}
+
+function get_anonymous_user_info()
+{
+    const yourname=document.getElementById("YourName").value;
+    const youremail=document.getElementById('YourEmail').value;
+    return {id:null, name:yourname, email:youremail};
 }
