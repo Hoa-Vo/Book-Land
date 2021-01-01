f449a9617403b37dae756d39668fec4de9664779
Hoa-Vo <dev.h1620@gmail.com>
2021-01-02 02:27:18 +0700

    fix style cart page

--- public/js/checkout.js
+++ public/js/checkout.js
@@ -19,4 +19,11 @@ function updateCheckOutHtml(books) {
   const source = $("#checkout-info").html();
   const template = Handlebars.compile(source);
   $("#info-list").html(template(books));
+  let totalMoney = 0;
+  for (let i = 0; i < books.length; i++) {
+    totalMoney += books[i].totalPrice;
+  }
+  console.log(totalMoney);
+  $("#total-money-pay").html(`${totalMoney} VND`);
+  $("#money-pay").html(`${totalMoney} VND`);
 }
