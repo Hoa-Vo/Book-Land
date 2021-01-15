2c1958ed42a67ea969a31341745362d25ea78719
AN VÕ THIỆN <voan0935995700@gmail.com>
2021-01-16 00:10:04 +0700

    redirect to login after register

--- controllers/verifyController.js
+++ controllers/verifyController.js
@@ -4,5 +4,5 @@ exports.verifyEndpoint = async (req, res, next) => {
   const id = req.params.id;
   await accountServices.vefifyEmail(id);
   console.log("OK verified email with id: " + id);
-  res.redirect("/");
+  res.redirect("/login");
 };
